fe62aef71731dfe121e1f4846b71c77d5b25c880
moualhi zine el abidine <zmoualhi@outlook.com>
2023-09-19 18:20:36 +0100

    fix 🐛: robot txt generation
    Signed-off-by: moualhi zine el abidine <zmoualhi@outlook.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), robotsTxt(), compress()],
   site: SITE_URL,
   base: '/portfolio',
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "dracula",
-      wrap: false,
-    },
-  },
+      wrap: false
+    }
+  }
 });
