87bb945245c1d98b982dc7cdf5e07e7156331bf7
Zine Moualhi 🇵🇸 <zmoualhi@outlook.com>
2025-02-23 18:53:01 +0100

    update og

--- src/config.ts
+++ src/config.ts
@@ -7,16 +7,10 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-<<<<<<< HEAD
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
-=======
   title: 'Zine El Abidine Moualhi',
   subtitle: 'My Web Site',
   // description: 'Container-dwelling sorcerer crafting Kubernetes and Terraform enchantments 🧙‍♂️ 🔮.',
   lang: 'en',       // 'en', 'zh_CN', 'zh_TW', 'ja'
->>>>>>> c50e4e5 (fix 🐛: merge conflicts)
   themeColor: {
     hue: 165,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
